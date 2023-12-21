62a0dcd447d03f1e2402f0430bf13f35a9e58f66
narcisolobo <narcisolobo@gmail.com>
2023-12-20 19:48:13 -0800

    week 4 day 3 notes and code

---
+++ 15-algorithms/coin-change.js
@@ -0,0 +1,28 @@
+/**
+ * Generate Coin Change
+ * Implement ​generateCoinChange(cents)​ that accepts
+ * a parameter for the number of cents, and computes
+ * how to represent that amount with the fewest
+ * number of coins. Return the result in the form of
+ * an object with the following keys: quarters, dimes,
+ * nickels, pennies.
+ */
+
+/**
+ * @typedef {Object} Change
+ * @property {number} quarters
+ * @property {number} dimes
+ * @property {number} nickels
+ * @property {number} pennies
+ */
+
+/**
+ * Given an amount of cents, returns an object
+ * representing that amount in the fewest number of
+ * coins.
+ * @param {number} cents
+ * @returns {Change}
+ */
+function generateCoinChange(cents) {
+  // your code here
+}
---
+++ 15-algorithms/factorial.js
@@ -0,0 +1,24 @@
+/**
+ * Factorial
+ * Write a function ​factorial(num)​ that, given a
+ * number, returns the product (multiplication)
+ * of all positive integers from 1 up to that
+ * number (inclusive).
+ *
+ * Examples:
+ * ​factorial(3)​ = 6 (or 1 * 2 * 3);
+ * factorial(5)​ = 120 (or 1 * 2 * 3 * 4 * 5).
+ */
+
+/**
+ * Returns the product of all positive integers
+ * from 1 up to that number (inclusive).
+ * @param {number} num
+ * @returns {number}
+ */
+function factorial(num) {
+  // your code here
+}
+
+console.log(factorial(3));
+console.log(factorial(5));
